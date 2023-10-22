56ee833846542761b3dd4d71d224145062e1760e
Carlo Pascoli <carlo.pascoli@gmail.com>
2023-10-22 17:15:50 +0100

    Clean up

--- contracts/interfaces/IUniswapV2Pair.sol
+++ contracts/interfaces/IUniswapV2Pair.sol
@@ -4,24 +4,12 @@ pragma solidity 0.8.19;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IUniswapV2Pair is IERC20 {
-    // event Approval(address indexed owner, address indexed spender, uint value);
-    // event Transfer(address indexed from, address indexed to, uint value);
-
-    // function name() external pure returns (string memory);
-    // function symbol() external pure returns (string memory);
-    function decimals() external view returns (uint8);
-    // function totalSupply() external view returns (uint);
-    // function balanceOf(address owner) external view returns (uint);
-    // function allowance(address owner, address spender) external view returns (uint);
-
-    // function approve(address spender, uint value) external returns (bool);
-    // function transfer(address to, uint value) external returns (bool);
-    // function transferFrom(address from, address to, uint value) external returns (bool);
 
     function DOMAIN_SEPARATOR() external view returns (bytes32);
     function PERMIT_TYPEHASH() external pure returns (bytes32);
-    function nonces(address owner) external view returns (uint);
 
+    function decimals() external view returns (uint8);
+    function nonces(address owner) external view returns (uint);
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
 
     event Mint(address indexed sender, uint amount0, uint amount1);
